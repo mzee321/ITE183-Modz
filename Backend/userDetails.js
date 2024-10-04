847102c00783171054fdffb4e301909e485ab126
mzee321 <158030875+mzee321@users.noreply.github.com>
2024-10-04 10:53:40 +0800

    ITE183 Modification
    wla nay description

---
+++ Backend/userDetails.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const UserDetailsScehma = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    userType: String,
+    college: String,
+  },
+  {
+    collection: "UserInfo",
+  }
+);
+
+mongoose.model("UserInfo", UserDetailsScehma);
